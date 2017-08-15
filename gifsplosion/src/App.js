f6d320fa245b901d7c3cb80a4e76c05b9da5b15f
Tom Conchie <tconchie@gmail.com>
2017-08-15 14:41:42 -0600

    Componetizes the aspects of the project into Header, Controls, GifList, and Gifs

--- gifsplosion/src/App.js
+++ gifsplosion/src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import Controls from './components/Controls';
+import Header from './components/Header';
+import GifList from './components/GifList';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <header className="header clearfix">
-          <h3 className="text-center">
-            <span className="head-first head-large">gif</span>
-            <span className="head-second head-large">SPLOSION</span>
-          </h3>
-        </header>
-        <Controls />
-      </div>
+      <div>
+      <Header />
+      <GifList />
+    </div>
     );
   }
 }
